cda553d9967d7924309ac1b7aaf3fe16f258f35d
mhyan3 <139754195+mhyan3@users.noreply.github.com>
2024-07-31 14:00:55 +0800

    使用 Microsoft.Office.Interop.Word 删除word文件中的超链接

---
+++ FrmwordLink.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class FrmwordLink
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(467, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "处理超链接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(367, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "选择文件夹";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(148, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(183, 21);
+            this.textBox1.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "转换前文件夹：";
+            // 
+            // FrmwordLink
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 178);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "FrmwordLink";
+            this.Text = "FrmLwordink";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ FrmwordLink.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+using Microsoft.Office.Interop.Word;
+namespace WindowsFormsApp1
+{
+    public partial class FrmwordLink : Form
+    {
+        /// <summary>
+        /// 处理pdf 的超链接 将超链接移除掉 
+        /// </summary>
+        public FrmwordLink()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string path = textBox1.Text;
+            if (string.IsNullOrEmpty(path))
+            {
+                MessageBox.Show("请选择转换前文件夹路径");
+                return;
+
+            }
+
+            DirectoryInfo dir = new DirectoryInfo(path);
+            // 遍历文件夹中的文件
+            foreach (FileInfo file in dir.GetFiles())
+            {
+
+                if (file.Extension.EndsWith(".doc", StringComparison.OrdinalIgnoreCase) || file.Extension.EndsWith(".docx", StringComparison.OrdinalIgnoreCase))
+                {
+                    GetLink(file.FullName);
+                } 
+            }
+
+
+            MessageBox.Show("成功");
+            
+
+        }
+
+        public void GetLink(string path)
+        {
+
+            Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application();
+            try
+            {
+                Document doc = wordApp.Documents.Open(path);
+
+                // 获取文档中的所有超链接  
+                foreach (Hyperlink link in doc.Hyperlinks)
+                {
+
+                    // 移除超链接  
+                    //   link.Range.Text = link.TextToDisplay; // 将超链接文本替换为显示文本  
+                    link.Delete(); // 删除超链接   
+                }
+                // 保存并关闭  
+                doc.Save();
+                doc.Close();
+                wordApp.Quit();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(doc);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(wordApp);
+
+            }
+            catch (Exception)
+            {
+
+                wordApp.Quit();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(wordApp);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog op = new FolderBrowserDialog();
+
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = op.SelectedPath;
+            }
+
+        }
+    }
+}
